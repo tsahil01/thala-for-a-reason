a049312a191171b77e270a3043346bc5ad6eefec
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-18 18:18:06 +0000

    cleared mess

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function getInputText(inputText){
-    if(inputText.length == 7 || inputText == 7){
+function getInputText(){
+    let inputData = document.getElementById("inputText").value;
+    if(inputData.length == 7 || inputData == 7){
         alert("THALA FOR A REASON")
     }else{
         alert("NOT A THALA")
