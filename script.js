00405a4082f3242aa975bd4bb949cdeab8cd8fa9
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-12-21 16:19:35 +0530

    halfway

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+let inputKey = document.getElementById("inputText")
+inputKey.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("submit-btn").click();
+    }
+});
+
 function getInputText(){
     let inputData = document.getElementById("inputText").value;
     if(inputData.length == 7 || inputData == 7){
-        alert("THALA FOR A REASON")
+        playThala("/src/thala.mp4")
+        if(inputData.length>1){
+            const reasonText = inputData.split("").join(" + ")
+            reasonText.concat(" = 7")
+            const finalResult = reasonText.concat(" = 7")
+            document.querySelector(".reason").innerHTML = `${finalResult}`
+        }
     }else{
         alert("NOT A THALA")
     }
 }
+
+
+function playThala(source){   
+    const videoElementclass = document.querySelectorAll('.thala-video')
+    videoElementclass.forEach( (element) =>{
+        const video = document.createElement('video')
+        element.appendChild(video)
+        video.setAttribute("src", `${source}`)
+        video.setAttribute("autoplay", true);
+        video.muted = true
+    } )
+}
+
+
